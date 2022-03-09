f9d5201873efb3b04d21b3b86b49a2a1a7154524
Aeris <jeanvicente641@gmail.com>
2022-03-09 17:04:52 +0100

    feat: first structure

--- yahtzee.js
+++ yahtzee.js
@@ -1,3 +1,24 @@
-exports.run = function run(lst) {
-    return;
+const formatRes = {
+    sum: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+    },
+    "Three of kind": 0,
+    "Four of kind": 0,
+    "Full House": 0,
+    "Small straight": 0,
+    "Large straight": 0,
+    "Chance": 0,
+    "YAHTZEE": 0
+};
+
+exports.run = function run(...lst) {
+    if(lst.length !== 5) return null;
+    console.log(lst);
+
+    return false;
 }
--- yahtzee.test.js
+++ yahtzee.test.js
@@ -1,5 +1,9 @@
 const yahtzee = require("./yahtzee.js");
 
-test('Yahtzee initial commit', () => {
-    expect(null).toBe(null);
+test('Yahtzee null', () => {
+    expect(yahtzee.run()).toBe(null);
+});
+
+test('Yahtzee first test', () => {
+    expect(yahtzee.run(1, 2, 3, 4, 5, 6)).toBe(null);
 });
