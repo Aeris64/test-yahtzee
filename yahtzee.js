394454a8a0d3cd22bcd8fd3d8e465813d76529cb
Aeris <jeanvicente641@gmail.com>
2022-03-09 17:19:26 +0100

    feat: yahtzee sum process

--- yahtzee.js
+++ yahtzee.js
@@ -1,24 +1,47 @@
-const formatRes = {
-    sum: {
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0,
-        5: 0,
-        6: 0,
-    },
-    "Three of kind": 0,
-    "Four of kind": 0,
-    "Full House": 0,
-    "Small straight": 0,
-    "Large straight": 0,
-    "Chance": 0,
-    "YAHTZEE": 0
-};
+class Yahtzee {
+    dices = [];
+    res = {
+        sum: {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            total: 0
+        },
+        "Three of kind": 0,
+        "Four of kind": 0,
+        "Full House": 0,
+        "Small straight": 0,
+        "Large straight": 0,
+        "Chance": 0,
+        "YAHTZEE": 0
+    }
+
+    constructor(allDices) {
+        this.dices = allDices;
+    }
+
+    Process() {
+        this.GetSum();
+    }
+
+    GetSum() {
+        for(let i = 0; i < this.dices.length; i++) {
+            this.res.sum.total += this.dices[i];
+            this.res.sum[this.dices[i]] += this.dices[i];
+        }
+    }
+}
 
 exports.run = function run(...lst) {
-    if(lst.length !== 5) return null;
-    console.log(lst);
+    if (lst.length !== 5) return null;
 
-    return false;
+    const y = new Yahtzee(lst);
+    y.Process();
+
+    console.log(y.res);
+
+    return y.res;
 }
--- yahtzee.test.js
+++ yahtzee.test.js
@@ -5,5 +5,22 @@ test('Yahtzee null', () => {
 });
 
 test('Yahtzee first test', () => {
-    expect(yahtzee.run(1, 2, 3, 4, 5, 6)).toBe(null);
+    expect(yahtzee.run(1, 2, 3, 4, 5)).toStrictEqual({
+        "Chance": 0,
+        "Four of kind": 0,
+        "Full House": 0,
+        "Large straight": 0,
+        "Small straight": 0,
+        "Three of kind": 0,
+        "YAHTZEE": 0,
+        "sum": {
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 0,
+            "total": 15
+        }
+    });
 });
